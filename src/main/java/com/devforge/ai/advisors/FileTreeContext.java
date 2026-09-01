41bb770f26a78eb62609837da970395bd299a052
swadesh-231 <swadeshchatterjee512@gmail.com>
2026-09-01 13:10:48 +0530

    add ai advisors

--- src/main/java/com/devforge/ai/advisors/FileTreeContext.java
+++ src/main/java/com/devforge/ai/advisors/FileTreeContext.java
@@ -1,5 +1,6 @@
 package com.devforge.ai.advisors;
 
+import com.devforge.dto.file.FileNode;
 import com.devforge.service.ProjectFileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,8 +9,16 @@ import org.springframework.ai.chat.client.ChatClientRequest;
 import org.springframework.ai.chat.client.ChatClientResponse;
 import org.springframework.ai.chat.client.advisor.api.StreamAdvisor;
 import org.springframework.ai.chat.client.advisor.api.StreamAdvisorChain;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.MessageType;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -17,8 +26,43 @@ import reactor.core.publisher.Flux;
 public class FileTreeContext implements StreamAdvisor {
     private final ProjectFileService projectFileService;
     @Override
-    public @NonNull Flux<ChatClientResponse> adviseStream(@NonNull ChatClientRequest chatClientRequest, @NonNull StreamAdvisorChain streamAdvisorChain) {
-        return null;
+    public @NonNull Flux<ChatClientResponse> adviseStream(@NonNull ChatClientRequest chatClientRequest,
+                                                          @NonNull StreamAdvisorChain streamAdvisorChain) {
+
+        Map<String, Object> context = chatClientRequest.context();
+        Long projectId = Long.parseLong(context.getOrDefault("projectId", 0).toString());
+        ChatClientRequest augmentedChatClientRequest = augmentRequestWithFileTree(chatClientRequest, projectId);
+
+        return streamAdvisorChain.nextStream(augmentedChatClientRequest);
+    }
+    private ChatClientRequest augmentRequestWithFileTree(ChatClientRequest request, Long projectId) {
+
+        List<Message> incomingMessages = request.prompt().getInstructions();
+
+        Message systemMessage = incomingMessages.stream()
+                .filter(m -> m.getMessageType() == MessageType.SYSTEM)
+                .findFirst()
+                .orElse(null);
+
+        List<Message> userMessages = incomingMessages.stream()
+                .filter(m -> m.getMessageType() != MessageType.SYSTEM)
+                .toList();
+
+        List<Message> allMessages = new ArrayList<>();
+        if (systemMessage != null) {
+            allMessages.add(systemMessage);
+        }
+
+        List<FileNode> fileTree = projectFileService.getFileTree(projectId).files();
+        String fileTreeContext = "\n\n ---- FILE_TREE ----\n"+fileTree.toString();
+        allMessages.add(new SystemMessage(fileTreeContext));
+
+        allMessages.addAll(userMessages);
+
+        return request
+                .mutate()
+                .prompt(new Prompt(allMessages, request.prompt().getOptions()))
+                .build();
     }
 
     @Override
